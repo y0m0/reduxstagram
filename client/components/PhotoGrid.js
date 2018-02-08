1a55652c15acf5c3aae8df748b319a60ba159616
y0m0 <lixi.daniele@gmail.com>
2018-02-08 21:57:06 +0100

    Photogrid map over the posts and generate all the photo components

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        I'am the photo grid
+        {this.props.posts.map((post, index) =>
+          <Photo
+            {...this.props}
+            key={index}
+            index={index}
+            post={post}
+          />
+        )}
       </div>
     )
   }
