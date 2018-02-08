ffc75a36c731b40143b381db6ca1ebe567d91f8b
y0m0 <lixi.daniele@gmail.com>
2018-02-09 00:11:42 +0100

    Use reducer composition for commentsReducer, add postComment reducer to handle the single posts and add a new comment

--- client/reducers/commentsReducer.js
+++ client/reducers/commentsReducer.js
@@ -1,11 +1,28 @@
-// a reducer needs 2 things:
-
-// 1. the action (info about what happend)
-// 2. a copy of the current state
+// this reducer uses reducer composition pattern
 
 function commentsReducer(state = [], action) {
-  console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+          user: action.user,
+          text: action.comment
+        }]
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
+}
 export default commentsReducer;
