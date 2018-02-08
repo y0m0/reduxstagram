04dab0fe3d33ff77d15a1adb457e19b4ebd946bb
y0m0 <lixi.daniele@gmail.com>
2018-02-08 23:50:37 +0100

    Add a form to add a single comment to the Post Component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -17,6 +17,11 @@ const Comments = React.createClass({
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
       </div>
     )
   }
