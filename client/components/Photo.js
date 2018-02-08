c37159619d06f8d81d1c2aca630373554e05792b
y0m0 <lixi.daniele@gmail.com>
2018-02-08 22:04:10 +0100

    Photo component can now display the single photos

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Photo = React.createClass({
   render() {
+    const { post, index, comments } = this.props;
+
     return (
       <figure className="grid-figure">
-        I am a photo
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img className="grid-photo" src={post.display_src} alt={post.caption} />
+          </Link>
+        </div>
       </figure>
     )
   }
